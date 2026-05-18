e1e25656d6b8e5f2678d3e48882cfff8ebf4517c
zklidn <zbluevampire08@gmail.com>
2026-05-19 06:21:47 +0800

    hallo

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -9,7 +9,7 @@ $routes->get('/home', 'Home::index');
 $routes->get('/about_us', 'Home::about_us');
 
 $routes->get('/darah/(:any)','HalamanAwal::darah_suci/$1');
-$routes->get('/cuaca/(:any)/(:any)','Dashboard::matahari/$1/$2');
+$routes->get('/cuaca/(:any)/(:any)','DashboardPMI::matahari/$1/$2');
 $routes->get('/tampilan','HalamanAwal::tampilanbaru');
 
 
