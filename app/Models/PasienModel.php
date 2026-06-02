402035ed8043e3a6cda244b0457a3f87dfa57e7f
Zikri <ikizikri37@gmail.com>
2026-06-02 01:17:09 -0700

    saya baru ambil alih database

--- app/Models/PasienModel.php
+++ app/Models/PasienModel.php
@@ -6,10 +6,17 @@ use CodeIgniter\Model;
 
 class PasienModel extends Model
 {
+
     protected $table      = 'pasien';
     protected $primaryKey = 'id_pasien';
-
-    protected $allowedFields = ['id_permintaan', 'nama', 'umur', 'diagnosa', 'golongan_darah'];
+    protected $useAutoIncrement = true;
+    protected $returnType = 'array';
+    protected $useSoftDeteles = true;
+    protected $protectFields = true;
+    protected $allowedFields = ['nama', 'umur', 'diagnosa', 'golongan_darah'];
 
     protected $useTimestamps = true;
+    protected $dateFormat = 'datetime';
+    protected $createFields = 'create_at';
+    protected $updateFields = 'update_at';
 }
--- app/Models/usermodels.php
+++ app/Models/usermodels.php
@@ -7,8 +7,14 @@ class UserModel extends Model
 {
     protected $table = 'users';
     protected $primaryKey = 'id';
-    
+    protected $useAutoIncrement = true;
+    protected $returnType = 'array';
+    protected $useSoftDeletes = true;
+    protected $protectFields = true;
     protected $allowedFields = ['name', 'email', 'password'];
 
     protected $useTimestamps = true;
+    protected $dateFormat = 'datetime';
+    protected $createFields = 'created_at';
+    protected $updateField = 'update_at';
 }
