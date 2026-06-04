32a066c38d4716e820ded9fdbd154fbe28128605
andikaheru46 <andikaheru46@gmail.com>
2026-06-04 15:51:34 +0800

    saya habis otak atik bagian migration sama model

--- app/Models/usermodels.php
+++ app/Models/usermodels.php
@@ -8,7 +8,7 @@ class UserModel extends Model
     protected $table = 'users';
     protected $primaryKey = 'id';
     
-    protected $allowedFields = ['name', 'email', 'password'];
+    protected $allowedFields = ['name', 'email', 'password', 'role'];
 
     protected $useTimestamps = true;
 }
